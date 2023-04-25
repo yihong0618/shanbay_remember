258b81ee991f05901e947d25e5977e46177cec22
BeautyyuYanli <beautyyuyanli@gmail.com>
2023-04-25 17:56:35 +0800

    define API body

--- api_wrapper.py
+++ api_wrapper.py
@@ -3,14 +3,22 @@ from typing import List, Any, Dict
 import subprocess, json
 class baseBackendAPI():
     @abstractmethod
-    def getWordList(self, kargv: Dict[str, Any])->List[Any]:
+
+    def getWordList(self, kargv: Dict[str, Any])->List[Dict[str, Any]]:
+    # Belike: 
+    # [
+    #     {
+    #         "word": "abandon",
+    #         "voice_url": "https://anything.com/abandon.mp3",
+    #     }
+    # ]
         pass
 
 class ShanbayAPI(baseBackendAPI):
     cookie: str
     def __init__(self, cookies: str):
         self.cookie = cookies
-    def getWordList(self, kargv: Dict[str, Any])->List[Any]:
+    def getWordList(self, kargv: Dict[str, Any])->List[Dict[str, Any]]:
         process = subprocess.Popen(
             ['node', './entrypoint.js', kargv.get("type"), self.cookie],
             stdout=subprocess.PIPE, 
@@ -18,7 +26,14 @@ class ShanbayAPI(baseBackendAPI):
             )
         stdout, stderr = process.communicate()
         print(stderr.decode('utf-8'))
-        return json.loads(stdout.decode('utf-8'))
+        raw = json.loads(stdout.decode('utf-8'))
+        return [
+            {
+                "word": i["vocab_with_senses"]["word"],
+                "voice_url": i["vocab_with_senses"]["sound"]["audio_us_urls"][0],
+            }
+            for i in raw
+        ]
 
 if __name__ == "__main__":
     with open('cookie.txt', 'r') as f:
